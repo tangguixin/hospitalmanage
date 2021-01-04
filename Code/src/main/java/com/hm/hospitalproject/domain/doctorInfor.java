f0fade9be67eab570ef8eb939c8005e80c2ff4f4
tangguixin <731875920@qq.com>
2021-01-05 00:22:08 +0800

    修改数据库

---
+++ Code/src/main/java/com/hm/hospitalproject/domain/doctorInfor.java
@@ -0,0 +1,7 @@
+package com.hm.hospitalproject.domain;/**
+ * Created with IntelliJ IDEA.
+ * @Auther: 唐贵欣
+ * @Date: 2021/01/05/0:13
+ * @Description: 
+ */public class DoctorInfor {
+}
---
+++ Code/src/main/java/com/hm/hospitalproject/repository/patient.java
@@ -0,0 +1,7 @@
+package com.hm.hospitalproject.repository;/**
+ * Created with IntelliJ IDEA.
+ * @Auther: 唐贵欣
+ * @Date: 2021/01/05/0:10
+ * @Description: 
+ */public class patient {
+}
